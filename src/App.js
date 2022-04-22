abf9a4546d85e4eccd5349a04977912c7b06777b
Windows 11 <shivershady@gmail.com>
2022-04-21 20:54:12 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./page/Home/Home";
+import Category from "./page/Category/Category";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header></Header>
+            <div className="container mx-auto">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home/>}></Route>
+                        <Route path="/category" element={<Category/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
 }
 
-export default App;
+export default App
